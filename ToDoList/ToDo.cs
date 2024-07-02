10c3a49663c1c7d1c639eae985fa3879639070e0
Shane Holman <Sholman@tql.com>
2024-07-02 11:01:48 -0400

    Add project files.

---
+++ ToDoList/ToDo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    internal class ToDo{
+
+        [StringLength(80)]
+        public string Description { get; set; } = string.Empty;
+
+       
+        public DateTime DueDate { get; set; }
+
+        public string Status { get; set; } = default (Active);
+
+        public string Priority { get; set; } = string.Empty;
+    }
+}
