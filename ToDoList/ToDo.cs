135af517c2a37df4273d99027dff452299be5a7b
Demetri Warren <dwarren@tql.com>
2024-07-02 11:11:54 -0400

    added Packages

--- ToDoList/ToDo.cs
+++ ToDoList/ToDo.cs
@@ -13,11 +13,12 @@ namespace ToDoList
         [StringLength(80)]
         public string Description { get; set; } = string.Empty;
 
-       
-        public DateTime DueDate { get; set; }
+        public DateTime DueDate { get; set; } = new DateTime();
 
+        [StringLength(30)]
         public string Status { get; set; } = "Active";
 
+        [StringLength(30)]
         public string Priority { get; set; } = string.Empty;
     }
 }
